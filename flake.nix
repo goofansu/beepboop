4a688236e1e34f31cd77a9dc839e4db943009537
Yejun Su <goofan.su@gmail.com>
2024-08-31 22:30:39 +0800

    nix flakes
    Install Elixir with specified Erlang: https://ejpcmac.net/blog/using-nix-in-elixir-projects/

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Exporing Phoenix LiveView";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.beam.packages.erlang_27.elixir_1_17 ];
+        };
+      });
+}
